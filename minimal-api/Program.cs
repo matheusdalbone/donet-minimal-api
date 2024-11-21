ce638e03c91f186da67bf94ec6ae1cd062b3aa72
Matheus Dalbone <matheusdalbone7@gmail.com>
2024-11-21 10:34:49 -0300

    feat: infraestrutura

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,3 +1,5 @@
+using minimal_api.Dominio.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -13,11 +15,3 @@ app.MapPost("/login", (LoginDTO loginDTO) => {
 
 
 app.Run();
-
-
-public class LoginDTO
-{
-  public string Email { get; set; } = default!;
-  public string Senha { get; set; } = default!;
-}
-
